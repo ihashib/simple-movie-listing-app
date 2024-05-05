4635d9e47dbefc176bf1fa5752ed3aea8723b403
ihashib <ihashib2@gmail.com>
2024-05-05 11:04:14 +0600

    movie and user class added

--- src/main/java/org/movie/Movie.java
+++ src/main/java/org/movie/Movie.java
@@ -1,4 +1,69 @@
 package org.movie;
 
 public class Movie {
+    private String title;
+    private String cast;
+    private String category;
+    private String releaseDate;
+    private String budget;
+
+    public Movie(String title, String cast, String category, String releaseDate, String budget) {
+        this.title = title;
+        this.cast = cast;
+        this.category = category;
+        this.releaseDate = releaseDate;
+        this.budget = budget;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setBudget(String budget) {
+        this.budget = budget;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", cast='" + cast + '\'' +
+                ", category='" + category + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", budget='" + budget + '\'' +
+                '}';
+    }
 }
+
--- src/main/java/org/movie/User.java
+++ src/main/java/org/movie/User.java
@@ -1,4 +1,45 @@
 package org.movie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String email;
+    private List<Movie> favorites;
+
+    public User(String email) {
+        this.email = email;
+        this.favorites = new ArrayList<>();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public List<Movie> getFavorites() {
+        return favorites;
+    }
+
+    public void addFavorite(Movie movie) {
+        if (!favorites.contains(movie)) {
+            favorites.add(movie);
+        }
+    }
+
+    public void removeFavorite(Movie movie) {
+        favorites.remove(movie);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", favorites=" + favorites +
+                '}';
+    }
 }
+
