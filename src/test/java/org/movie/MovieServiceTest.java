d3eaa643f7d303c486fe22b85dbc8834dc448773
ihashib <ihashib2@gmail.com>
2024-05-05 12:18:43 +0600

    completed test cases

--- src/test/java/org/movie/MovieServiceTest.java
+++ src/test/java/org/movie/MovieServiceTest.java
@@ -2,6 +2,10 @@ package org.movie;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class MovieServiceTest {
 
     private MovieService movieService;
@@ -9,31 +13,46 @@ public class MovieServiceTest {
     @Before
     public void setup() {
         movieService = new MovieService();
-        // TODO
+        movieService.addMovie(new Movie("The Shawshank Redemption", "Morgan Freeman", "Drama", "1994", "9.3"));
+        movieService.addMovie(new Movie("The Godfather", "Marlon Brando", "Crime", "1972", "9.2"));
+        movieService.addMovie(new Movie("The Dark Knight", "Christian Bale", "Action", "2008", "9.0"));
     }
 
     @Test
     public void testSearchMoviesByTitle() {
-        // TODO
+        List<Movie> results = movieService.searchMovies("Shawshank", "title");
+        assertEquals(1, results.size());
+        assertEquals("The Shawshank Redemption", results.get(0).getTitle());
     }
 
     @Test
     public void testSearchMoviesByCast() {
-        // TODO
+        List<Movie> results = movieService.searchMovies("Morgan Freeman", "cast");
+        assertEquals(1, results.size());
+        assertEquals("The Shawshank Redemption", results.get(0).getTitle());
     }
 
     @Test
     public void testSearchMoviesByCategory() {
-        // TODO
+        List<Movie> results = movieService.searchMovies("Action", "category");
+        assertEquals(1, results.size());
+        assertTrue(results.stream().anyMatch(m -> m.getTitle().equals("The Dark Knight")));
     }
 
     @Test
     public void testGetMovieDetails() {
-        // TODO
+        Movie movie = movieService.getMovieDetails("The Godfather");
+        assertNotNull(movie);
+        assertEquals("The Godfather", movie.getTitle());
+        assertEquals("Marlon Brando", movie.getCast());
     }
 
     @Test
     public void testAddMovieToFavorites() {
-        // TODO
+        User user = movieService.registerUser("ihashib2@gmail.com");
+        Movie movie = movieService.getMovieDetails("The Godfather");
+
+        movieService.addMovieToFavorites("ihashib2@gmail.com", movie);
+        assertTrue(user.getFavorites().contains(movie));
     }
 }
--- src/test/java/org/movie/UserServiceTest.java
+++ src/test/java/org/movie/UserServiceTest.java
@@ -3,6 +3,8 @@ package org.movie;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class UserServiceTest {
 
     private MovieService movieService;
@@ -10,36 +12,72 @@ public class UserServiceTest {
     @Before
     public void setup() {
         movieService = new MovieService();
-        // TODO
+        movieService.addMovie(new Movie("The Godfather", "Marlon Brando", "Crime", "1972", "9.2"));
+        movieService.addMovie(new Movie("The Dark Knight", "Christian Bale", "Action", "2008", "9.0"));
     }
 
     @Test
     public void testSuccessfulUserRegistration() {
-        // TODO
+        User user = movieService.registerUser("ihashib2@gmail.com");
+
+        assertNotNull(user);
+        assertEquals("ihashib2@gmail.com", user.getEmail());
+        assertTrue(user.getFavorites().isEmpty());
     }
 
     @Test
     public void testAddMovieToFavorites() {
-        // TODO
+        User user = movieService.registerUser("ihashib2@gmail.com");
+        Movie movie = movieService.getMovieDetails("The Godfather");
+
+        movieService.addMovieToFavorites("ihashib2@gmail.com", movie);
+
+        assertTrue(user.getFavorites().contains(movie));
+        assertEquals(1, user.getFavorites().size());
     }
 
     @Test
     public void testRemoveMovieFromFavorites() {
-        // TODO
+        User user = movieService.registerUser("ihashib2@gmail.com");
+        Movie movie = movieService.getMovieDetails("The Dark Knight");
+        user.addFavorite(movie);
+
+        movieService.removeMovieFromFavorites("ihashib2@gmail.com", movie);
+
+        assertFalse(user.getFavorites().contains(movie));
+        assertTrue(user.getFavorites().isEmpty());
     }
 
     @Test
     public void testGetFavorites() {
-        // TODO
+        User user = movieService.registerUser("ihashib2@gmail.com");
+        Movie movie1 = movieService.getMovieDetails("The Godfather");
+        Movie movie2 = movieService.getMovieDetails("The Dark Knight");
+        user.addFavorite(movie1);
+        user.addFavorite(movie2);
+
+        assertEquals(2, user.getFavorites().size());
+        assertTrue(user.getFavorites().contains(movie1));
+        assertTrue(user.getFavorites().contains(movie2));
     }
 
     @Test
     public void testAddDuplicateMovieToFavorites() {
-        // TODO
+        User user = movieService.registerUser("ihashib2@gmail.com");
+        Movie movie = movieService.getMovieDetails("The Godfather");
+        user.addFavorite(movie);
+        user.addFavorite(movie); // Adding the same movie twice
+
+        assertEquals(1, user.getFavorites().size());
     }
 
     @Test
     public void testRemovingNonExistentMovieFromFavorites() {
-        // TODO
+        User user = movieService.registerUser("ihashib2@gmail.com");
+        Movie nonExistentMovie = new Movie("Non-Existent Movie", "", "", "", "");
+
+        movieService.removeMovieFromFavorites("ihashib2@gmail.com", nonExistentMovie);
+
+        assertTrue(user.getFavorites().isEmpty());
     }
 }
